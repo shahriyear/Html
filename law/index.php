4e5ecc9b232c34c34ce12bf2e827ca92c75230c4
shahriyear <shahriyear@gmail.com>
2018-02-13 02:24:03 +0600

    lawer site
    html, css , sass

---
+++ law/index.php
@@ -0,0 +1,445 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+<link rel="stylesheet" type="text/css" href="css/animate.css-master/animate.min.css">
+<link rel="stylesheet" type="text/css" href="css/Hover-master/css/hover-min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="js/jQ.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/custom.js"></script>
+<title>WorkPro</title>
+</head>
+
+<body>
+
+<!---header start-->
+<header class="animated bounce">
+  <section id="menubar">
+    <nav class="navcls">
+      <div class="container">
+        <div class="row">
+          <div class="navbar-header navhead">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar iconcls"></span> <span class="icon-bar iconcls"></span> <span class="icon-bar iconcls"></span> </button>
+            <h1 class="navbar-brand"><big>Law</big></h1>
+          </div>
+          <div id="navbar"class="collapse navbar-collapse clscen">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="#"class="hvr-underline-from-left">Home</a></li>
+              <li><a href="#aboutus" class="hvr-underline-from-left">About </a></li>
+              <li><a href="#about" class="hvr-underline-from-left">Why us</a></li>
+              <li><a href="#good" class="hvr-underline-from-left">Best</a></li>
+              <li><a href="#area" class="hvr-underline-from-left">Practices</a></li>
+              <li><a href="#price" class="hvr-underline-from-left">Payment</a></li>
+              <li><a href="#contact" class="hvr-underline-from-left">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </section>
+</header>
+<!--header end--> 
+
+<!--slider start-->
+<section id="slider">
+  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel"> 
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    </ol>
+    
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active slideimg"> <img src="img/slide111.jpg" class="img-responsive" alt="">
+        <div class="carousel-caption"> <big><strong>Call us for free Advice</strong></big> </div>
+      </div>
+      <div class="item  slideimg"> <img src="img/slide000.jpg" class="img-responsive" alt="">
+        <div class="carousel-caption"> </div>
+      </div>
+    </div>
+    
+    <!-- Controls --> 
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next"> <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> </div>
+</section>
+
+<!--slider end--> 
+
+<!--aboutus start-->
+<section id="aboutus">
+	<div class="row">
+    	<div class="container">
+        	<div class="col-md-6">
+            	<p class="pone"><strong>Something About Us</strong></p>
+                <h1><strong>Engaging Design </strong>with great marketing results for lawyers</h1>
+                <p class="ptwo">The template has been designed to help you create an outstanding, elegant and professional LAW Website.</p>
+                <p class="pthree">You can edit this Template easily, without writing a single line of code. All typography and graphic elements are included.</p>
+            </div>
+            
+            <div class="col-md-6">
+            	<div class="row">
+                <div class="col-md-6 about-img">
+                	<img src="img/ab1.jpg" class="img-responsive">
+                    
+                </div>
+                <div class="col-md-6 about-img">
+                	<img src="img/ab2.jpg" class="img-responsive">
+                    
+                </div>
+                <div class="col-md-6 about-img">
+                	<img src="img/ab3.jpg" class="img-responsive">
+                    
+                </div>
+                <div class="col-md-6 about-img">
+                	<img src="img/Paweł-Magieras-Desk-e1406542968164.jpg" class="img-responsive">
+                    
+                </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<!--aboutus end-->
+
+
+<!--about start-->
+<section id="about">
+  <div class="row">
+    <div class="container">
+   <h1 style="text-align:center">Why Us</h1>
+      <div class="col-md-4">
+        <div class="about-sec">
+          <div class="about-sce-first"> <i class="fa fa-university fa-2x"></i> </div>
+          <h3>We Are Reliable</h3>
+          <p>Lorem ipsum dolor sit amet, consecur adipiscing elit. Donec gravida, ipsum eget magna dignissim dolor dignissim.</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="about-sec">
+          <div class="about-sce-first"> <i class="fa fa-briefcase fa-2x"></i> </div>
+          <h3> We Have Reputation </h3>
+          <p>Lorem ipsum dolor sit amet, consecur adipiscing elit. Donec gravida, ipsum eget magna dignissim dolor dignissim.</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="about-sec">
+          <div class="about-sce-first"> <i class="fa fa-gavel fa-2x"></i> </div>
+          <h3>We Get Results</h3>
+          <p>Lorem ipsum dolor sit amet, consecur adipiscing elit. Donec gravida, ipsum eget magna dignissim dolor dignissim.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--about end--> 
+
+<!--meet the team start-->
+<section id="team">
+  <div class="row">
+    <div class="container">
+      <h1>Our Attorneys</h1>
+      <p>Meet Out Talented Team</p>
+      <div class="col-md-3">
+        <div class="team-sec" >
+          <div class="team-sec-img"> <img src="img/law1.jpg" class="img-responsive"> </div>
+          <h1>James Ferner</h1>
+          <p>Civil law</p>
+          <p><a class="fa fa-facebook" href="#fb"></a><a href="#tt" class="fa fa-twitter"></a><a href="link" class="fa fa-linkedin"></a><a href="#gg" class="fa fa-google-plus"></a><a href="#tw" class="fa fa-twitch"></a> </p>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="team-sec" >
+          <div class="team-sec-img"> <img src="img/law2.jpg" class="img-responsive"> </div>
+          <h1>Harold Lee</h1>
+          <p>Civil lawyer</p>
+          <p><a class="fa fa-facebook" href="#fb"></a><a href="#tt" class="fa fa-twitter"></a><a href="link" class="fa fa-linkedin"></a><a href="#gg" class="fa fa-google-plus"></a><a href="#tw" class="fa fa-twitch"></a> </p>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="team-sec" >
+          <div class="team-sec-img"> <img src="img/law3.jpg" class="img-responsive"> </div>
+          <h1>Lana Anderson</h1>
+          <p>Civil lawyer</p>
+          <p><a class="fa fa-facebook" href="#fb"></a><a href="#tt" class="fa fa-twitter"></a><a href="link" class="fa fa-linkedin"></a><a href="#gg" class="fa fa-google-plus"></a><a href="#tw" class="fa fa-twitch"></a> </p>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="team-sec" >
+          <div class="team-sec-img"> <img src="img/law4.jpg" class="img-responsive"> </div>
+          <h1>Williamson Dee</h1>
+          <p>Civil lawyer</p>
+          <p><a class="fa fa-facebook" href="#fb"></a><a href="#tt" class="fa fa-twitter"></a><a href="link" class="fa fa-linkedin"></a><a href="#gg" class="fa fa-google-plus"></a><a href="#tw" class="fa fa-twitch"></a> </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--meet the team end-->
+
+<!--good start-->
+<section id="good">
+	<div class="row">
+    	<div class="container">
+        <h1>You’re in<strong> good company</strong></h1>
+        <p>Our results speak for themselves. Read through some of our client case studies <br>to learn more about our innovative work.</p>
+        	<div class="col-md-3 good-main">
+            <h1>3,584</h1>
+            <p>SOLVED CASES</p>
+            	
+            </div>
+            <div class="col-md-3 good-main">
+            <h1>6,368</h1>
+            <p>SUCCESSFUL CASES</p>
+            	
+            </div>
+            <div class="col-md-3 good-main">
+            <h1>225</h1>
+            <p>REGISTERED CASES</p>
+            	
+            </div>
+            <div class="col-md-3 good-main">
+            <h1>9,846</h1>
+            <p>CASE STUDIES</p>
+            	
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--good end-->
+<hr class="hrcls"/>
+<!--testimonials start-->
+<section id="test">
+	<div class="row">
+    	<div class="container">
+       <!-- <h1>Hare some <strong>Testimonials</strong> from our clients</h1>-->
+        	<div class="test-sce">
+            <p><strong>“If there’s one thing you learn by working on a lot of different Web sites, it’s that almost any design idea–no matter how appallingly bad–can be made usable in the right circumstances, with enough effort.”</strong></p>
+            <h3>  ― Steve Krug</h3>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--testimonials end-->
+
+
+<!--area start-->
+<section id="area">
+	<div class="row">
+    	<div class="container">
+		<h1>Practice Areas</h1>
+        	<div class="col-md-3">
+            	<div class="sec-img">
+                	<img src="img/area1.jpg" class="img-responsive">
+                   
+                </div> <p>Insurance Claims</p>
+            
+            </div>
+            
+            <div class="col-md-3">
+            	<div class="sec-img">
+                	<img src="img/area2.jpg" class="img-responsive">
+                  
+                </div>  <p>Vehicle accidents</p>
+            
+            </div>
+            
+            <div class="col-md-3">
+            	<div class="sec-img">
+                	<img src="img/area3.jpg" class="img-responsive">
+                    
+                </div><p>Medical Malpractice</p>
+            
+            </div>
+            
+            <div class="col-md-3">
+            	<div class="sec-img">
+                	<img src="img/area4.jpg" class="img-responsive">
+                   
+                </div> <p>Premises Liability </p>
+            
+            </div>
+            <div class="col-md-3">
+            	<div class="sec-img">
+                	<img src="img/area1.jpg" class="img-responsive">
+                   
+                </div> <p>Insurance Claims</p>
+            
+            </div>
+            
+            <div class="col-md-3">
+            	<div class="sec-img">
+                	<img src="img/area2.jpg" class="img-responsive">
+                   
+                </div>
+             <p>Vehicle accidents</p>
+            </div>
+            
+            <div class="col-md-3">
+            	<div class="sec-img">
+                	<img src="img/area3.jpg" class="img-responsive">
+                  
+                </div>  <p>Medical Malpractice</p>
+            
+            </div>
+            
+            <div class="col-md-3">
+            	<div class="sec-img">
+                	<img src="img/area4.jpg" class="img-responsive">
+                   
+                </div> <p>Premises Liability </p>
+            
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<!--area end-->
+
+<!--price table start-->
+<section id="price">
+	<div class="row">
+    	<div class="container">
+        <h1>Payment Plans</h1>
+        	<div class="col-md-3">
+            	<div class="price-sec">
+                	<div class="price-sec-head">
+                    	<h1><strong>30$</strong></h1>
+                        <p>Overview</p>
+                    </div>
+                    <p>voluptarem accusantium</p>
+                    <p>magnam aliquam voluptas</p>
+                    <p>suscipit laboriosam</p>
+                    <p>commodi cionsequatur</p>
+                 
+                    <input type="button" class="btncls" value="grab it">
+                </div>
+            </div>
+            <div class="col-md-3">
+            	<div class="price-sec">
+                	<div class="price-sec-head">
+                    	<h1><strong>50$</strong></h1>
+                        <p>Information</p>
+                    </div>
+                    <p>voluptarem accusantium</p>
+                    <p>magnam aliquam voluptas</p>
+                    <p>suscipit laboriosam</p>
+                    <p>commodi cionsequatur</p>
+                 
+                    <input type="button" class="btncls" value="grab it">
+                </div>
+            </div>
+            <div class="col-md-3">
+            	<div class="price-sec">
+                	<div class="price-sec-head">
+                    	<h1><strong>70$</strong></h1>
+                        <p>Advice</p>
+                    </div>
+                    <p>voluptarem accusantium</p>
+                    <p>magnam aliquam voluptas</p>
+                    <p>suscipit laboriosam</p>
+                    <p>commodi cionsequatur</p>
+                 
+                    <input type="button" class="btncls" value="grab it">
+                </div>
+            </div>
+            <div class="col-md-3">
+            	<div class="price-sec">
+                	<div class="price-sec-head">
+                    	<h1><strong>90$</strong></h1>
+                        <p>Representation</p>
+                    </div>
+                    <p>voluptarem accusantium</p>
+                    <p>magnam aliquam voluptas</p>
+                    <p>suscipit laboriosam</p>
+                    <p>commodi cionsequatur</p>
+                 
+                    <input type="button" class="btncls" value="grab it">
+                </div>
+            </div>	
+        </div>
+    </div>
+</section>
+<!--price table end-->
+
+<!--contact start-->
+<section id="contact">
+	<div class="row">
+    <h1>Need Legal Assistance?</h1>
+    <p>LAW is a Perfect Law / Attorney Template</p>
+    	<div class="container">
+        	<div class="contact-form col-md-12" >
+            <form class="form-horizontal" role="form" method="post" action="index.php">
+    <div class="form-group">
+        
+        <div class="col-md-6 col-md-offset-3">
+            <input type="text" class="form-control" id="name" name="name" placeholder="Name" value="">
+        </div>
+    </div>
+    <div class="form-group">
+        
+        <div class="col-md-6 col-md-offset-3">
+            <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="">
+        </div>
+    </div><div class="form-group">
+        
+        <div class="col-md-6 col-md-offset-3">
+            <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" value="">
+        </div>
+    </div>
+    <div class="form-group">
+        
+        <div class="col-md-6 col-md-offset-3">
+            <textarea class="form-control" rows="4" name="message" placeholder="Message"></textarea>
+        </div>
+    </div>
+    
+    <div class="form-group">
+        <div class="col-md-2 col-md-offset-5">
+            <input id="submit" name="submit" type="submit" value="Contact Us Now" class="btncls">
+        </div>
+    </div>
+    
+</form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--contact end-->
+
+<!--footer start-->
+<footer id="foot">
+	<div class="row">
+    	<div class="container">
+        	<div class="col-md-7">
+            	<h1>  All Rights Reserved &copy; Law	 </h1>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!--footer end-->
+
+<!--<section class="test" style="height:500px; background:#C7B6B6"> </section>-->
+<script type="text/javascript">
+$(window).scroll(function() {
+if ($(this).scrollTop() >1){  
+    $('header').addClass("sticky");
+  }
+  else{
+    $('header').removeClass("sticky");
+  }
+});
+</script>
+</body>
+</html>
---
+++ law/js/custom.js
@@ -0,0 +1,6 @@
+// jQuery(document).ready(function($){
+	
+// 	$('a').click(function(e){
+// 		e.preventdefault();
+// 	})
+// });
